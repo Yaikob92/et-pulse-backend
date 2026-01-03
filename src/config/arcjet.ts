1ca7b3ca10331b7068a3026bacfa80fb80019569
Yaikob/92 <wyaikob@gmail.com>
2026-01-03 09:38:44 +0300

    implement allowing postman in arcject middleware

--- src/config/arcjet.ts
+++ src/config/arcjet.ts
@@ -2,23 +2,23 @@ import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
 
 // initialize Arcjet with security rules
 export const aj = arcjet({
-    key: process.env.ARCJET_KEY!,
-    characteristics: ["ip.src"],
-    rules: [
-        // shield protects your app from common attacks e.g SQL injection, XSS, CSRF token
-        shield({ mode: "LIVE" }),
+  key: process.env.ARCJET_KEY!,
+  characteristics: ["ip.src"],
+  rules: [
+    // shield protects your app from common attacks e.g SQL injection, XSS, CSRF token
+    shield({ mode: "LIVE" }),
 
-        // bot detection - block all bots except search engines
-        detectBot({
-            mode: "LIVE",
-            allow: ["CATEGORY:SEARCH_ENGINE"],
-        }),
-        // rate limiting with token bucket algorithim
-        tokenBucket({
-            mode: "LIVE",
-            refillRate: 10,
-            interval: 10,
-            capacity: 15,
-        }),
-    ],
+    // bot detection - block all bots except search engines
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+    // rate limiting with token bucket algorithim
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 10,
+      interval: 10,
+      capacity: 15,
+    }),
+  ],
 });
--- src/middleware/arcjet.middleware.ts
+++ src/middleware/arcjet.middleware.ts
@@ -2,27 +2,37 @@ import { aj } from "../config/arcjet.js";
 import { NextFunction, Request, Response } from "express";
 import { isSpoofedBot } from "@arcjet/inspect";
 
-export const arcjetMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
-    const decision = await aj.protect(req, { requested: 5 })
-    console.log("Arcjet decision", decision)  //.conclusion
+export const arcjetMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void | Response> => {
+  const userAgent = req.headers["user-agent"] || "";
 
-    if (decision.isDenied()) {
-        if (decision.reason.isRateLimit()) {
-            return res.status(429).json({ error: "Too Many Requests" });
-        } else if (decision.reason.isBot()) {
-            return res.status(403).json({ error: "No bots allowed" });
-        } else {
-            return res.status(403).json({ error: "Forbidden" });
-        }
+  // allow Postman (Dev Only)
+  if (userAgent.includes("PostmanRuntime")) {
+    return next();
+  }
+  const decision = await aj.protect(req, { requested: 5 });
+  console.log("Arcjet decision", decision); //.conclusion
+
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return res.status(429).json({ error: "Too Many Requests" });
     }
-
-    if (decision.ip.isHosting()) {
-        return res.status(403).json({ error: "Forbidden" });
+    if (decision.reason.isBot()) {
+      return res.status(403).json({ error: "No bots allowed" });
     }
+    return res.status(403).json({ error: "Forbidden" });
+  }
 
-    if (decision.results.some(isSpoofedBot)) {
-        return res.status(403).json({ error: "Forbidden" });
-    }
+  if (decision.ip.isHosting()) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
 
-    next();
+  if (decision.results.some(isSpoofedBot)) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+
+  next();
 };
